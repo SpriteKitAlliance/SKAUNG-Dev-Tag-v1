3c8c10d0a6630f65f3aa19c6d6a1aa35668e5e0c
marcJV <marc.vandehey@gmail.com>
2017-07-24 16:13:10 -0500

    press to jump, hold to jump a lot more.

---
+++ SKAUNG Dev Tag v1/Protocols/Updateable.swift
@@ -0,0 +1,13 @@
+//
+//  Updateable.swift
+//  SKAUNG Dev Tag v1
+//
+//  Created by Marc Vandehey on 7/24/17.
+//  Copyright © 2017 SpriteKit Alliance. All rights reserved.
+//
+
+import SpriteKit
+
+protocol Updateable {
+  func update(deltaTime: TimeInterval)
+}
